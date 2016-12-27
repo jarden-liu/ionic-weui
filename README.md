fcb823c3727682c618823a8307f04308c9945d82
liujunjiang <liujunjiang@szxhinfo.com>
2016-12-27 15:20:40 +0800

    update version to 1.1.0

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # ionic-weui
 
-weui for ionic
+在WeUI官方版本基础上适配ionic框架，提供了js版本。
 
+## 用法
+  将dist目录下的weui.js引入到你的项目中，或者将src目下的weui.css引入到你的项目中（二选一）。
+
+  具体使用办法，`参照官方文档`[weui](https://github.com/weui/weui/wiki)。
 
 ## Links
 
--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,21 @@ var css2js = require("gulp-css2js");
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var download = require("gulp-download");
+var replace = require('gulp-replace');
+var runSequence = require('run-sequence');
 
 
 var path = {
     css: './src/weui.css'
 };
+
+var baseUrl = 'https://raw.githubusercontent.com/weui/weui/master/dist/style/weui.css';
+
+
+gulp.task('default', function(callback) {
+    runSequence('download', 'update', 'css2js', callback);
+});
 
 
 
@@ -21,6 +31,20 @@ gulp.task('css2js', function() {
             extname: '.min.js'
         }))
         .pipe(gulp.dest("./dist/"));
+});
+
+
+
+gulp.task('download', function() {
+    return download(baseUrl)
+        .pipe(gulp.dest("src/"));
+});
+
+
+gulp.task('update', function() {
+    return gulp.src(path.css)
+        .pipe(replace(/html(.|\n)*@font-face/, '\/*针对Ionic做了部分修正*\/\n@font-face'))
+        .pipe(gulp.dest("src/"));
 });
 
 
